702c8c41ecdd4b990c238df273346c2338ecdbca
Abraão Alcântara <sephioh@gmail.com>
2016-08-19 18:47:51 -0300

    conversor converte I, II, III e V

---
+++ src/conversor.php
@@ -0,0 +1,6 @@
+<?php
+function conversor() {
+	return 1;
+}
+
+?>
--- tests/MyTest.php
+++ tests/MyTest.php
@@ -1,11 +1,28 @@
 <?php
 use PHPUnit\Framework\TestCase;
 
+function conversor($numeroRomano) {
+    $numeroDeOcorrenciasDeI = substr_count($numeroRomano, 'I'); 
+    $numeroDeOcorrenciasDeV = substr_count($numeroRomano, 'V');
+	return $numeroDeOcorrenciasDeI + (5 * $numeroDeOcorrenciasDeV);
+}
 
-class MyTest extends TestCase
+class ConversorTest extends TestCase
 {
-    public function testSimpleTest()
+    public function testConverterIRetorna1()
     {
-        $this->assertFalse(True);
+        $this->assertEquals(1, conversor('I'));
     }
+	public function testConverterIIRetorna2()
+	{
+		$this->assertEquals(2, conversor('II'));
+	}
+	public function testConverterIIIRetorna3()
+	{
+		$this->assertEquals(3, conversor('III'));
+	}
+	public function testConverterVRetorna5()
+	{
+		$this->assertEquals(5, conversor('V'));
+	}
 }
