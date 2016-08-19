268e3e6d953723c32f8ddc9227858136add410d6
Abraão Alcântara <sephioh@gmail.com>
2016-08-19 20:23:07 -0300

    Adicionado mais testes

--- tests/MyTest.php
+++ tests/MyTest.php
@@ -2,27 +2,73 @@
 use PHPUnit\Framework\TestCase;
 
 function conversor($numeroRomano) {
-    $numeroDeOcorrenciasDeI = substr_count($numeroRomano, 'I'); 
-    $numeroDeOcorrenciasDeV = substr_count($numeroRomano, 'V');
-	return $numeroDeOcorrenciasDeI + (5 * $numeroDeOcorrenciasDeV);
+    $ultimaLetra = null;
+    $total = 0;
+
+    $arrayNumeroRomanos = str_split($numeroRomano);
+
+    foreach ($arrayNumeroRomanos as $letraAtual) {
+      if ($ultimaLetra == null) {
+        $total += valorDoCaracterRomano($letraAtual);
+      }
+      else {
+        if(valorDoCaracterRomano($ultimaLetra) < valorDoCaracterRomano($letraAtual)){
+          $total += valorDoCaracterRomano($letraAtual) - valorDoCaracterRomano($ultimaLetra) - 1;
+        }
+        else {
+          $total += valorDoCaracterRomano($letraAtual);
+        }
+      }
+      $ultimaLetra = $letraAtual;
+    }
+    return $total;
 }
 
-class ConversorTest extends TestCase
-{
-    public function testConverterIRetorna1()
-    {
-        $this->assertEquals(1, conversor('I'));
+function valorDoCaracterRomano($caracterRomano) {
+  return array(
+    "I" => 1,
+    "V" => 5,
+    "X" => 10,
+  )[$caracterRomano];
+}
+
+
+class ValorDoCaracterRomanoTest extends TestCase {
+    public function testRecebeIRetorna1(){
+      $this->assertEquals(1, valorDoCaracterRomano('I'));
     }
-	public function testConverterIIRetorna2()
-	{
+    public function testRecebeVRetorna5(){
+      $this->assertEquals(5, valorDoCaracterRomano('V'));
+    }
+    public function testRecebeXRetorna10(){
+      $this->assertEquals(10, valorDoCaracterRomano('X'));
+    }
+}
+
+
+class ConversorTest extends TestCase {
+  public function testConverterIRetorna1() {
+      $this->assertEquals(1, conversor('I'));
+  }
+
+	public function testConverterIIRetorna2() {
 		$this->assertEquals(2, conversor('II'));
 	}
-	public function testConverterIIIRetorna3()
-	{
+
+	public function testConverterIIIRetorna3() {
 		$this->assertEquals(3, conversor('III'));
 	}
-	public function testConverterVRetorna5()
-	{
+
+  public function testConverterIVRetorna4(){
+    $this->assertEquals(4, conversor('IV'));
+  }
+
+	public function testConverterVRetorna5() {
 		$this->assertEquals(5, conversor('V'));
 	}
+
+  public function testConverterIXRetorna9() {
+		$this->assertEquals(9, conversor('IX'));
+	}
+
 }
